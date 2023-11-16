89d1e30c5aea975cf1f6d05867b1529ae8a58ee3
Arun Pathivada <arunpathivada143@gmail.com>
2023-11-16 09:25:22 +0530

    changed origin as * to accept reqests form alll sources

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ import postRoute from "./routes/postRoute.js";
 dotenv.config();
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-    optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true,
+    optionsSuccessStatus: 204,
   };
 app.use(cors(corsOptions));
 app.use(express.json({limit:'50mb'}));
