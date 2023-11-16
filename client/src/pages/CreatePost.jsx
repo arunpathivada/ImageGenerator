fcbbc8cb5787b06f9099c561e8af62f19caf362b
Arun Pathivada <arunpathivada143@gmail.com>
2023-11-16 11:28:36 +0530

    error solved

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       SetLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://imagegenerator1.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -62,7 +62,7 @@ const CreatePost = () => {
   try {
     setGeneratingImg(true);
 
-    const response = await fetch('http://localhost:8080/api/v1/dalle', {
+    const response = await fetch('https://imagegenerator1.onrender.com/api/v1/dalle', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     const fetchPosts = async ()=>{
       setLoading(true);
       try{
-        const response = await fetch("http://localhost:8080/api/v1/post",
+        const response = await fetch("https://imagegenerator1.onrender.com/api/v1/post",
         {
           method:"GET",
           headers:{
